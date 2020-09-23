9d835e7c5e378aac0476ca78102713960e91a10c
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-09-23 22:07:04 +0200

    Legger inn riktig DobbeltLenketListe.java

--- src/DobbeltLenketListe.java
+++ src/DobbeltLenketListe.java
@@ -1,63 +1,155 @@
+import java.util.Comparator;
 import java.util.Iterator;
 
+//hentet fra tilleggsklasser som fulgte med oppgaven 
+
 public class DobbeltLenketListe<T> implements Liste<T> {
-    @Override
-    public boolean leggInn(T verdi) {
-        return false;
+
+    /**
+     * Node class
+     * @param <T>
+     */
+    private static final class Node<T> {
+        private T verdi;                   // nodens verdi
+        private Node<T> forrige, neste;    // pekere
+
+        private Node(T verdi, Node<T> forrige, Node<T> neste) {
+            this.verdi = verdi;
+            this.forrige = forrige;
+            this.neste = neste;
+        }
+
+        private Node(T verdi) {
+            this(verdi, null, null);
+        }
     }
 
-    @Override
-    public void leggInn(int indeks, T verdi) {
+    // instansvariabler
+    private Node<T> hode;          // peker til den første i listen
+    private Node<T> hale;          // peker til den siste i listen
+    private int antall;            // antall noder i listen
+    private int endringer;         // antall endringer i listen
 
+    public DobbeltLenketListe() {
+        throw new UnsupportedOperationException();
     }
 
-    @Override
-    public boolean inneholder(T verdi) {
-        return false;
+    public DobbeltLenketListe(T[] a) {
+        throw new UnsupportedOperationException();
     }
 
-    @Override
-    public T hent(int indeks) {
-        return null;
-    }
-
-    @Override
-    public int indeksTil(T verdi) {
-        return 0;
-    }
-
-    @Override
-    public T oppdater(int indeks, T verdi) {
-        return null;
-    }
-
-    @Override
-    public boolean fjern(T verdi) {
-        return false;
-    }
-
-    @Override
-    public T fjern(int indeks) {
-        return null;
+    public Liste<T> subliste(int fra, int til){
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int antall() {
-        return 0;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean tom() {
-        return false;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean leggInn(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void leggInn(int indeks, T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean inneholder(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T hent(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indeksTil(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T oppdater(int indeks, T nyverdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean fjern(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T fjern(int indeks) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void nullstill() {
+        throw new UnsupportedOperationException();
+    }
 
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String omvendtString() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
-}
+
+    public Iterator<T> iterator(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+    private class DobbeltLenketListeIterator implements Iterator<T>
+    {
+        private Node<T> denne;
+        private boolean fjernOK;
+        private int iteratorendringer;
+
+        private DobbeltLenketListeIterator(){
+            denne = hode;     // p starter på den første i listen
+            fjernOK = false;  // blir sann når next() kalles
+            iteratorendringer = endringer;  // teller endringer
+        }
+
+        private DobbeltLenketListeIterator(int indeks){
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasNext(){
+            return denne != null;
+        }
+
+        @Override
+        public T next(){
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void remove(){
+            throw new UnsupportedOperationException();
+        }
+
+    } // class DobbeltLenketListeIterator
+
+    public static <T> void sorter(Liste<T> liste, Comparator<? super T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+} // class DobbeltLenketListe
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+    //Oppgave 0 - no runtime error
     public static void main(String[] args) {
         Liste<String> liste = new DobbeltLenketListe<>();
     }
