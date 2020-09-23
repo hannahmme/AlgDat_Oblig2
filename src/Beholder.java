0cd4a08fed9bdf203c64b0f3d7bc7d8e50b450c4
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-09-23 20:54:34 +0200

    Oppretter prosjekt. Legger inn interface Liste og Beholder

--- src/Beholder.java
+++ src/Beholder.java
@@ -6,11 +6,17 @@ import java.util.function.Predicate;
 
 public interface Beholder<T> extends Iterable<T> {
     public boolean leggInn(T t);       // legger inn t i beholderen
+
     public boolean inneholder(T t);    // sjekker om den inneholder t
+
     public boolean fjern(T t);         // fjerner t fra beholderen
+
     public int antall();               // returnerer antallet i beholderen
+
     public boolean tom();              // sjekker om beholderen er tom
+
     public void nullstill();           // tømmer beholderen
+
     public Iterator<T> iterator();     // returnerer en iterator
 
     default boolean fjernHvis(Predicate<? super T> p)  // betingelsesfjerning
--- src/Liste.java
+++ src/Liste.java
@@ -3,16 +3,27 @@ import java.util.Iterator;
 //Hentet fra https://www.cs.hioa.no/~ulfu/appolonius/kap3/2/kap32.html#3.2.1
 public interface Liste<T> extends Beholder<T> {
     public boolean leggInn(T verdi);           // Nytt element bakerst
+
     public void leggInn(int indeks, T verdi);  // Nytt element på plass indeks
+
     public boolean inneholder(T verdi);        // Er verdi i listen?
+
     public T hent(int indeks);                 // Hent element på plass indeks
+
     public int indeksTil(T verdi);             // Hvor ligger verdi?
+
     public T oppdater(int indeks, T verdi);    // Oppdater på plass indeks
+
     public boolean fjern(T verdi);             // Fjern objektet verdi
+
     public T fjern(int indeks);                // Fjern elementet på plass indeks
+
     public int antall();                       // Antallet i listen
+
     public boolean tom();                      // Er listen tom?
+
     public void nullstill();                   // Listen nullstilles (og tømmes)
+
     public Iterator<T> iterator();             // En iterator
 
     public default String melding(int indeks)  // Unntaksmelding
