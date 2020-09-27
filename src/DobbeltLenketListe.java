9226b25f0a202f431a5c002813fc903a1e55f04c
Amalie Leiknes <s340559@oslomet.no>
2020-09-27 22:22:52 +0200

    Strukturert metodene etter rekkefølge på oppgavene
    Skrevet inn kommentarer på hvilken oppgave skallkoden gjelder så vi har bedre oversikt på hvem som skal skrive hvilken kode.

--- src/DobbeltLenketListe.java
+++ src/DobbeltLenketListe.java
@@ -34,69 +34,30 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         throw new UnsupportedOperationException();
     }
 
-    public DobbeltLenketListe(T[] a) {
-        throw new UnsupportedOperationException();
-    }
 
-    public Liste<T> subliste(int fra, int til){
-        throw new UnsupportedOperationException();
-    }
 
+
+
+// Oppgave 1
+    // metoden skal returnere antall verdier
     @Override
     public int antall() {
         throw new UnsupportedOperationException();
     }
 
+    // metoden skal returnere true/ false om den er tom eller ikke
     @Override
     public boolean tom() {
         throw new UnsupportedOperationException();
     }
 
-    @Override
-    public boolean leggInn(T verdi) {
+    // metoden skal lage en dobbeltlenket liste med verdiene fra tabell a
+    public DobbeltLenketListe(T[] a) {
         throw new UnsupportedOperationException();
     }
 
-    @Override
-    public void leggInn(int indeks, T verdi) {
-        throw new UnsupportedOperationException();
-    }
 
-    @Override
-    public boolean inneholder(T verdi) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public T hent(int indeks) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int indeksTil(T verdi) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public T oppdater(int indeks, T nyverdi) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public T fjern(int indeks) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void nullstill() {
-        throw new UnsupportedOperationException();
-    }
-
+    // Oppgave 2a
     @Override
     public String toString() {
         throw new UnsupportedOperationException();
@@ -106,11 +67,89 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         throw new UnsupportedOperationException();
     }
 
+
+// Oppgave 2b
+    @Override
+    public boolean leggInn(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+
+
+
+
+
+    // Oppgave 3a
+    // denne metoden skal returnere noden med den gitte indeksen/posisjonen
+    /*private Node<T> finnNode(int indeks){
+
+    }*/
+
+    @Override
+    public T oppdater(int indeks, T nyverdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T hent(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    // Oppgave 3b
+    public Liste<T> subliste(int fra, int til){
+        throw new UnsupportedOperationException();
+    }
+
+
+    // Oppgave 4
+    @Override
+    public int indeksTil(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean inneholder(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    //Oppgave 5
+    @Override
+    public void leggInn(int indeks, T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+
+// Oppgave 6
+    @Override
+    public boolean fjern(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public T fjern(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    // Oppgave 7
+    @Override
+    public void nullstill() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    //8b: skal returnere en instans av iteratorklassen
     @Override
     public Iterator<T> iterator() {
         throw new UnsupportedOperationException();
     }
 
+    // 8d: sjekk om indeksen er lovlig. se så oppgavetekst
     public Iterator<T> iterator(int indeks) {
         throw new UnsupportedOperationException();
     }
@@ -121,26 +160,32 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         private boolean fjernOK;
         private int iteratorendringer;
 
+        // tror ikke denne skal endres?
         private DobbeltLenketListeIterator(){
             denne = hode;     // p starter på den første i listen
             fjernOK = false;  // blir sann når next() kalles
             iteratorendringer = endringer;  // teller endringer
         }
 
+        // 8c: sette pekeren denne til noden som hører til den oppgitte indeksen
         private DobbeltLenketListeIterator(int indeks){
             throw new UnsupportedOperationException();
         }
 
+        // NB!! Denne koden skal ikke endres
         @Override
         public boolean hasNext(){
             return denne != null;
         }
 
+    // 8a: se oppgavetekst
         @Override
         public T next(){
             throw new UnsupportedOperationException();
         }
 
+
+        // Oppgave 9
         @Override
         public void remove(){
             throw new UnsupportedOperationException();
@@ -148,8 +193,14 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     } // class DobbeltLenketListeIterator
 
+
+
+    // Oppgave 10: ikke bruk hjelpestrukturer
     public static <T> void sorter(Liste<T> liste, Comparator<? super T> c) {
         throw new UnsupportedOperationException();
     }
+
+
+
 
 } // class DobbeltLenketListe
