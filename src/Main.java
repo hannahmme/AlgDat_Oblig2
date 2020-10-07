5250dc7e9b2bb450294d7e4f94405afe250b51ba
Caroline Jetteberg <s313564@oslomet.no>
2020-10-07 15:41:25 +0200

    Testa oppg 8 i Main

--- src/Main.java
+++ src/Main.java
@@ -52,6 +52,15 @@ public class Main {
 
         System.out.println(liste.tom());
 
+        //Oppgave 8
+        /*String[] navn = {"Lars", "Anders", "Bodil", "Kari", "Per", "Berit"};
+        Liste<String> liste1 = new DobbeltLenketListe<>(navn);
+        liste1.forEach(s -> System.out.print(s + " "));
+        System.out.println();
+        for (String s : liste1){
+            System.out.print(s + " ");
+        }*/
+
         //Oppgave 10
         Liste<Integer> genericList = new DobbeltLenketListe<>(helttallsliste);
         DobbeltLenketListe.sorter(genericList, Comparator.naturalOrder());
